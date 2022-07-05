7154f688217286c8b5c76fc424dc2e81c7de677d
riulwoo <geun10121@naver.com>
2022-07-05 18:16:42 +0900

    220705 add code client socket/ chat

---
+++ drawing-app/chatclient.js
@@ -0,0 +1,46 @@
+const socket = io();
+
+socket.on('update', function(data){
+    var chat = document.getElementById('chat')
+    var message = document.createElement('div')
+    var node = document.createTextNode(`${data.name}: ${data.message}`)
+    var className = ''
+
+    // 타입에 따라 적용할 클래스를 다르게 지정
+    switch(data.type){
+        case 'message':
+            className = 'other'
+            break
+        case 'connect':
+            className = 'connect'
+            break
+        case 'disconnect':
+            className = 'disconnect'
+            break
+    }
+
+    message.classList.add(className)
+    message.appendChild(node)
+    chat.prepend(message)
+    msg.focus();
+})
+
+function send(){
+    // 입력되어있는 데이터 가져오기
+    var message = document.getElementById('text').value
+
+    // 가져왔으니 데이터 빈칸으로 변경
+    document.getElementById('text').value = ''
+
+    // 내가 전송할 메시지 클라이언트에게 표시
+    var chat = document.getElementById('chat')
+    var msg = document.createElement('div')
+    var node = document.createTextNode(message)
+    msg.classList.add('me')
+    msg.appendChild(node)
+    chat.prepend(msg)
+    msg.focus();
+
+    // 서버로 message 이벤트 전달 + 데이터
+    socket.emit('message', {type: 'message', message: message})
+}
--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 // server.js
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const fs = require('fs');
 
+/* 설치한 express 모듈 불러오기 */
+const express = require('express')
+ 
+/* 설치한 socket.io 모듈 불러오기 */
+const socket = require('socket.io')
+ 
+/* Node.js 기본 내장 모듈 불러오기 */
+const http = require('http')
+ 
+/* Node.js 기본 내장 모듈 불러오기 */
+const fs = require('fs')
+ 
+/* express 객체 생성 */
+const app = express()
+
+/* express http 서버 생성 */
+const server = http.createServer(app)
+ 
+/* 생성된 서버를 socket.io에 바인딩 */
+const io = socket(server)
+
+app.use(express.static('drawing-app'))
 
 server.listen(process.env.PORT || 8080, ()=> {
   console.log("서버가 대기중입니다.");
+})
+
+app.get('/', function(req, res){
+  fs.readFile('drawing-app/index.html', function(err, data){
+    if(err){
+      res.send('에러')
+    }else{
+      res.writeHead(200, {'Content-Type':'text/html'})
+      res.write(data)
+      res.end()
+    }
+  })
 })
