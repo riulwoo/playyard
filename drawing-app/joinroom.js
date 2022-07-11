51071eb519dc1a14c8c608aab917d26f3d0daaf3
Riulwoo <geun10121@naver.com>
2022-07-11 15:13:38 +0900

    ... client code

--- drawing-app/joinroom.js
+++ drawing-app/joinroom.js
@@ -1,23 +1,43 @@
 let myId;
 let usercnt = 0;
+let beforebtn = null;
+let currentbtn = null;
+let roomnum = null;
 
 socket.on('userid', (data)=> {
     myId = data;
 })
 
-function joinroom(room){
-    let roomnum = document.getElementById(room)
+
+
+function joinroom(room, btn){
+    beforebtn = currentbtn;
+    currentbtn = document.getElementById(btn);
+    roomnum = document.getElementById(room)
+
+
+    if(beforebtn != null)
+    {
+        beforebtn.style.display = 'block';
+        currentbtn.style.display = 'none';
+    }
+    else
+        currentbtn.style.display = 'none';
+        
     socket.emit('joinroom', 
     {
         id : myId,
         room : room
     })
 
+}
+
 socket.on('roomcnt', (roomcnt)=> {
     usercnt = roomcnt;
-})
 
-roomnum.innerText = `( ${usercnt} / 2명 접속중 )`;
+    roomnum.innerText = `( ${usercnt} / 2명 접속중 )`;
 
     ctx.clearRect(0,0,canvas.clientWidth, canvas.clientHeight)
-}
+
+})
+
