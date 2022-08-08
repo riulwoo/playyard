b46772b8706ce22e6bd0b6cc038ba1b0e786c417
mins <80028813+minseong00@users.noreply.github.com>
2022-08-08 08:19:18 +0000

    ...

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ io.on('connection', (socket) => {
 
   //사이트 접속 해제
   socket.on('disconnect', (reason) => { // 1.roominfo 배열 index 2.roominfo 안에 id 객체에 비교 3. 비교 후 해당 객체의 index와 roominfo의 
-    const id = info();
+    const index = info();
     console.log(id);
     if (id !== undefined)
-      roominfo[id[0]].id[id[1]] = null;
+      roominfo[index[0]].id[index[1]] = null;
     console.log(`${socket.id}님이 ${reason}의 이유로 퇴장하셨습니다.`)
   })
 
@@ -86,9 +86,9 @@ io.on('connection', (socket) => {
     if (full.length == 2) socket.emit('fail'); //1-1. 꽉찼다면 실패 메시지
     else { //1-2. 덜찼다면 덜찬 인덱스 확인
       try {
-        const user = info();    //2. 방을 옮기는 것인지 처음 방에 입장하는 것인지 확인
-        roominfo[user[0]].id[user[1]] = null;
-        socket.leave(roominfo[id[0]].room); //    1. 유저가 있었던 방의 인덱스에서 일치하는 아이디를 삭제, leave
+        const index = info();    //2. 방을 옮기는 것인지 처음 방에 입장하는 것인지 확인
+        roominfo[index[0]].id[index[1]] = null;
+        socket.leave(roominfo[index[0]].room); //    1. 유저가 있었던 방의 인덱스에서 일치하는 아이디를 삭제, leave
         rooms[pIndex]--;
         console.log('방옮김');
       } catch { } finally {
