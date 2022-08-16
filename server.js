afee6ff60ddbefd7752217ceee529546f2bf7fe7
mins <80028813+minseong00@users.noreply.github.com>
2022-08-16 06:02:19 +0000

    ...

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
 
   function info() { //1. 룸인포 인덱스랑 유저 아이디 인덱스를 가져와야한다
     const roomIndex = roominfo.findIndex(i => i.id == socket.id);
-    const idIndex = roominfo.indexOf(socket.id, (e, i) => {
+    const idIndex = roominfo.indexOf(socket.id, (e) => {
       return e.id;
     });
     console.log(idIndex);
@@ -84,7 +84,7 @@ io.on('connection', (socket) => {
     else { //1-2. 덜찼다면 덜찬 인덱스 확인
       try {
         const Index = info();    //2. 방을 옮기는 것인지 처음 방에 입장하는 것인지 확인
-        if (Index[1] == -1) {
+        if (Index[1] !== -1) {
           roominfo[Index[0]].id[Index[1]] = null;
           socket.leave(roominfo[Index[0]].room); //    1. 유저가 있었던 방의 인덱스에서 일치하는 아이디를 삭제, leave
           rooms[Index[0]] = rooms[Index[0]] - 1;
