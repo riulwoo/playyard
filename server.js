6e8aabd5bb64fcff0168ae647798c83b35f096eb
mins <80028813+minseong00@users.noreply.github.com>
2022-08-18 14:27:04 +0000

    ...

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ io.on('connection', (socket) => {
         socket.join(roominfo[cIndex].room);
         console.log(`들어갈 방 Index : ${cIndex}`);
         console.log(`해당 방의 빈 자리 : ${idIndex}`);
-        roominfo[cIndex].id[idIndex] = id;
+        Object.values(roominfo[cIndex].id)[idIndex] = id;
         rooms[cIndex] += 1;
         console.log(`해당 방의 현황 : ${roominfo[cIndex].id}`);
         console.log(`전체 인원 배열 : ${rooms}`);
